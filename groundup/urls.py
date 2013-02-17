732ac89c82624168f6c81cacd8747b75dc053bf6
Taylor Savage <tjsavage@stanford.edu>
2013-02-16 22:30:46 -0800

    Mapped out basic site

--- groundup/urls.py
+++ groundup/urls.py
@@ -6,7 +6,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'info.views.home', name='home'),
     # url(r'^groundup/', include('groundup.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -14,4 +13,13 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    
+)
+
+urlpatterns += patterns('django.contrib.flatpages.views',
+    url(r'^about/$', 'flatpage', {'url': '/about/'}, name="about"),
+    url(r'^contact/$', 'flatpage', {'url': '/contact/'}, name="contact"),
+    url(r'^facts/$', 'flatpage', {'url': '/facts/'}, name="facts"),
+    url(r'^$', 'flatpage', {'url': '/'}, name='home'),
+
 )
--- info/models.py
+++ info/models.py
@@ -1,5 +1,38 @@
 from django.db import models
 
 class Barista(models.Model):
-    name = models.TextField(max_length=40)
-    image = models.ImageField(upload_to="baristas/")
+    name = models.CharField(max_length=40)
+    image = models.ImageField(upload_to="baristas/", null=True, blank=True)
+    bio = models.TextField()
+    
+    def __unicode__(self):
+        return self.name
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField(upload_to="posts/", null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __unicode__(self):
+        return "%s: %s" % (self.date_added, self.title)
+
+class Vendor(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    image = models.ImageField(upload_to="vendors/", null=True, blank=True)
+
+    def __unicode__(self):
+        return "%s" % self.name
+        
+class MenuItem(models.Model):
+    CATEGORY_CHOICES = (
+        ('F', "Food"),
+        ('D', "Drinks"),
+        ('O', "Other"),
+    )
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(blank=True)
+    image = models.ImageField(upload_to="items/", null=True, blank=True)
+    category = models.CharField(max_length=2, choices=CATEGORY_CHOICES)
